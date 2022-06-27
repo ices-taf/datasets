a9b0f5c042eea8698fa9bf4a3be337df5eb2f9aa
Colin Millar <colin.millar@ices.dk>
2022-06-27 11:27:29 +0200

    add VMS data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # datasets
+
 A collection of R scripts to download uesful datasets in TAF analyses
+
+1. VMS.R - download vms data by c-square
---
+++ vms.R
@@ -0,0 +1,33 @@
+#' VMS data from the ICES VMS and Logbook Database
+#'
+#' VMS data from the ICES VMS and Logbook Database
+#'
+#' @name vms
+#' @format csv file
+#' @tafOriginator ICES
+#' @tafYear 2020
+#' @tafAccess Restricted
+#' @tafSource script
+
+library(icesTAF)
+library(icesVMS)
+library(icesVocab)
+
+# find the ecoregion of interest
+ecoregions <-
+  gsub(
+    " Ecoregion", "",
+    getCodeList("Ecoregion")$Description
+  )
+
+baltic_eco <- grep("Baltic", ecoregions, value = TRUE)
+
+# download
+# note to access download a token for the current R session using
+# update_token({ices username})
+
+# to get all years (year = 0)
+vms <- get_vms(ecoregion = baltic_eco, year = 2009)
+
+# save
+write.taf(vms)
